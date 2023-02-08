9dc526dc5107157d8d9f6733d2bb7ad2bac863c6
Gareth Whiting <83248823+ggwhiting@users.noreply.github.com>
2023-02-08 17:47:28 +0000

    Added functionality to the CipherManager class
    This class has the ability to return a String[] of macs, ciphers, signatures and message digests.

--- CipherManager.java
+++ CipherManager.java
@@ -4,14 +4,23 @@ import java.util.*;
 public class CipherManager { 
 
 	private Set<String> ciphers;
+	private Set<String> signatures;
+	private Set<String> messageDigests;
+	private Set<String> macs;
 
 	public CipherManager(){
 		this.ciphers = null;
+		this.signatures = null;
+		this.messageDigests = null;
+		this.macs = null;
 	}
 
 
 	public void probe() throws Exception {
+		this.ciphers = Security.getAlgorithms("Mac");
 		this.ciphers = Security.getAlgorithms("Cipher");
+                this.ciphers = Security.getAlgorithms("Signature");
+                this.ciphers = Security.getAlgorithms("MessageDigest");
 	}
 
 	public String[] getCiphers() throws Exception { 
@@ -23,5 +32,36 @@ public class CipherManager {
 		}
 		return buffer;
 	}
+
+
+	public String[] getMacs() throws Exception {
+                Object[] macsObjects = this.macs.toArray();
+                String[] buffer = new String[macsObjects.length];
+                for(int i = 0;i < macsObjects.length;i++){
+                        buffer[i] = new String();
+                        buffer[i] = macsObjects[i].toString();
+                }
+                return buffer;
+        }
+
+	public String[] getSignatures() throws Exception {
+               	Object[] signaturesObjects = this.signatures.toArray();
+                String[] buffer = new String[signaturesObjects.length];
+                for(int i = 0;i < signaturesObjects.length;i++){
+                        buffer[i] = new String();
+                        buffer[i] = signaturesObjects[i].toString();
+                }
+                return buffer;
+        }
+
+	public String[] getDigests() throws Exception {
+                Object[] mdObjects = this.messageDigests.toArray();
+                String[] buffer = new String[mdObjects.length];
+                for(int i = 0;i < mdObjects.length;i++){
+                        buffer[i] = new String();
+                        buffer[i] = mdObjects[i].toString();
+                }
+                return buffer;
+        }
 }
 
