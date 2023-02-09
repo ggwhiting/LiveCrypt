2453bfb060c425941397b0f13d43c8a90da45645
Gareth Whiting <83248823+ggwhiting@users.noreply.github.com>
2023-02-09 07:23:57 +0000

    Test for CipherCoord class
    Basic stdout functionality test.  Nothing fancy.

---
+++ CipherCoordTest.java
@@ -0,0 +1,14 @@
+
+
+
+
+public class CipherCoordTest  {
+
+	public static void main(String[] args) throws Exception {
+		CipherCoord coord = new CipherCoord();
+		coord.init();
+		coord.toTerm();
+	
+	}
+
+}
